b9a8831fd7b16d463a93b3bc970cf7a41c91dda9
Professor Agora <agoraprofessor@gmail.com>
2021-12-09 23:06:10 +0530

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -42,7 +42,7 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"⚡Successfully installed⚡ {i}")
+            print(f"⚡Say thanks to Professor⚡ {i}")
     except Exception as e:
         print(f"Error while installing requirments {str(e)}")
 
